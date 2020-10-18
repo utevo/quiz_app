6ac4e6e47495402c6ce17e6d7ac4d743371364a5
utevo <utevo@pop-os.localdomain>
2020-10-18 20:47:47 +0200

    feat: add style to Answer widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -8,9 +8,11 @@ class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       child: RaisedButton(
         child: Text('Answer'),
         onPressed: _onPressed,
+        color: Colors.cyanAccent,
       ),
     );
   }
