a5329d1f0c6bb8e169e200ff61114471d3ec64f1
utevo <utevo@pop-os.localdomain>
2020-10-18 19:24:39 +0200

    feat: add state

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,25 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+var questions = [
+  'Your favourite animal?',
+  'Your favourite food',
+];
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIdx = 0;
+
+  ansewerQuestion() {
+    setState(() {
+      questionIdx = questionIdx + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,18 +32,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('Question nr 1'),
+            Text(questions[questionIdx]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: ansewerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: ansewerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: ansewerQuestion,
             ),
           ],
         ),
